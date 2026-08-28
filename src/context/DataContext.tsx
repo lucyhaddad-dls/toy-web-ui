092209e8d09ebb7c8208c7a1b0d6aad08a071441
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-08-28 14:26:08 +0000

    hopefully no error on DataContext now

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,4 +1,24 @@
-import { createContext } from "react"
-import type { DataContextType } from "../models/models"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createContext, type SetStateAction } from "react"
+import type { DataContextType, SampleResponseKeys, SampleUnitKeys, SampleValueResponse, TotalAbsorptionDataset, UnitValue } from "../models/models"
 
-export const DataContext = createContext<DataContextType>({})
+export const DataContext = createContext<DataContextType>({
+    sampleValues: [],
+    setSampleValues: function (value: SetStateAction<SampleValueResponse[]>): void {
+        throw new Error("Function not implemented.")
+    },
+    sampleUnits: [],
+    setSampleUnits: function (value: SetStateAction<UnitValue[]>): void {
+        throw new Error("Function not implemented.")
+    },
+    getSingleValue: function (name: SampleResponseKeys): string | null {
+        throw new Error("Function not implemented.")
+    },
+    getUnit: function (name: SampleUnitKeys): UnitValue {
+        throw new Error("Function not implemented.")
+    },
+    allAbsorptionData: undefined,
+    setAllAbsorptionData: function (value: SetStateAction<TotalAbsorptionDataset>): void {
+        throw new Error("Function not implemented.")
+    }
+})
