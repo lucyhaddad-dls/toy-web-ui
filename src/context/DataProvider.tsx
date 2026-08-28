51c573cc1d4d261d03a1f112f8dcf8acc10642ab
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-08-28 09:31:37 +0000

    add defaults

--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -2,23 +2,22 @@ import type React from "react";
 import { DataContext } from "./DataContext";
 import { useEffect, useState } from "react";
 import { getSampleData } from "../models/queryFunctions";
-import { type SampleValueResponse } from "../models/models";
+import { type SampleValueResponse, type UnitValue } from "../models/models";
+import { defaultSampleUnits } from "../models/defaults";
 
 export function DataProvider(props: { children: React.ReactNode }) {
     const { children } = props;
 
     const [sampleValues, setSampleValues] = useState<SampleValueResponse[]>([])
 
+    const [sampleUnits, setSampleUnits] = useState<UnitValue[]>(defaultSampleUnits)
+
     useEffect(() => {
     let ignore = false;
     getSampleData().then(data => {
-      if (!ignore) {
-        setSampleValues(data);
-      }
-    });
-        return () => {
-        ignore = true;
-        };
+      if (!ignore) { setSampleValues(data); }
+        });
+        return () => { ignore = true; }
     }, []);
 
 
---
+++ src/models/defaults.ts
@@ -0,0 +1,18 @@
+import type { SampleValueResponse, UnitValue } from "./models";
+
+
+export const defaultSampleUnits: UnitValue[] = [
+    { name: "mass_unit", value: "g", options: ["kg", "g", "mg", "ug"] },
+    { name: "length_unit", value: "cm", options: ["m", "cm", "mm", "um"] },
+    { name: "energy_unit", value: "kev", options: ["gev", "kev", "ev"] },
+  ];
+
+  export const nullSampleValues: SampleValueResponse[] = [
+    {name:"formula", value:{val:null, dtype:"str"}},
+    {name:"absorber", value:{val:null, dtype:"str"}},
+    {name:"edge", value:{val:null, dtype:"str"}},
+    {name:"density", value:{val:null, dtype:"float"}},
+    {name:"area", value:{val:null, dtype:"float"}},
+    {name:"thickness", value:{val:null, dtype:"float"}},
+    {name:"mass", value:{val:null, dtype:"float"}}
+  ]
