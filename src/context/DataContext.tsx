d4a02a1bbf39b1d6b47b64fb4b1013c597e42b8a
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-08-28 14:29:00 +0000

    remove unused vars. from DataContext..

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,24 +1,23 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { createContext, type SetStateAction } from "react"
-import type { DataContextType, SampleResponseKeys, SampleUnitKeys, SampleValueResponse, TotalAbsorptionDataset, UnitValue } from "../models/models"
+import { createContext} from "react"
+import type { DataContextType, UnitValue } from "../models/models"
 
 export const DataContext = createContext<DataContextType>({
     sampleValues: [],
-    setSampleValues: function (value: SetStateAction<SampleValueResponse[]>): void {
+    setSampleValues: function (): void {
         throw new Error("Function not implemented.")
     },
     sampleUnits: [],
-    setSampleUnits: function (value: SetStateAction<UnitValue[]>): void {
+    setSampleUnits: function (): void {
         throw new Error("Function not implemented.")
     },
-    getSingleValue: function (name: SampleResponseKeys): string | null {
+    getSingleValue: function (): string | null {
         throw new Error("Function not implemented.")
     },
-    getUnit: function (name: SampleUnitKeys): UnitValue {
+    getUnit: function (): UnitValue {
         throw new Error("Function not implemented.")
     },
     allAbsorptionData: undefined,
-    setAllAbsorptionData: function (value: SetStateAction<TotalAbsorptionDataset>): void {
+    setAllAbsorptionData: function (): void {
         throw new Error("Function not implemented.")
     }
 })
