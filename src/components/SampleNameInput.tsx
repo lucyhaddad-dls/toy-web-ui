ab1bbfe73ec6df3b77e925ad16fa3afa07e009d7
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-09-07 11:16:47 +0000

    change positioning of save sample menu

--- src/components/SampleNameInput.tsx
+++ src/components/SampleNameInput.tsx
@@ -4,29 +4,35 @@ import Fade from "@mui/material/Fade";
 
 export function SetNameBox(props: {onName:(name: string) => void}) {
     const [open, setOpen] = useState<boolean>(false)
-    
-    const onClick = () => {
+    const [menuPosition, setMenuPosition] = useState<HTMLElement|null>(null)
+
+
+    const onClick = (event: React.MouseEvent<HTMLElement>|null) => {
         setOpen(!open)
+        if (!open && event !=null){
+            setMenuPosition(event.currentTarget)
+        }
+        else {setMenuPosition(null)}
 
     }
 
     return (
-        <Stack>
+        <Stack >
             <Button variant="contained" sx={{bgcolor:"#477a51"}}
             onClick = {onClick}>Save as New Sample</Button>
-
             <Popper id = "0" open={open}
-            style={{ position: 'fixed',
-             bottom: "50%", right: "50%", 
-             top: 'unset', left: 'unset' }}>
+            anchorEl={menuPosition}
+            placement="right">
            <Fade in={open}>
-            <Box sx={{ border:1, p:1, bgcolor:"#fefefe" }}>
+            <Box
+             sx={{ border:1, p:1, bgcolor:"white",
+              borderColor:"#477a51"}} >
             <TextField label="Sample Name: "
             onKeyUp = {(event) => {if(event.key == "Enter"){
             const val = event.target as HTMLTextAreaElement
+            onClick(null)
             if (val.value != ""){
             props.onName(val.value);
-            onClick();
             event.preventDefault()}}}}/>
             </Box>
             </Fade>
