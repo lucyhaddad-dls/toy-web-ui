d00b8dd7d7f9df9768a4cf21cac1f6fa6129909b
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-09-14 10:38:58 +0000

    add menu for adding properties

--- src/new/pages/EditPropertiesPage.tsx
+++ src/new/pages/EditPropertiesPage.tsx
@@ -1,18 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SampleDataContext } from "../../context/SampleContext";
-import { Grid, Stack, Typography } from "@mui/material";
+import { Button, Grid, Menu, MenuItem, Stack, Typography } from "@mui/material";
 import { nullSampleValues } from "../../models/defaults";
 import { SamplePropertyInput } from "../components/SamplePropertyInput";
 import AddIcon from '@mui/icons-material/Add';
+import { sampleKeys } from "../../models/models";
 
 export function EditPropertiesPage () {
     const { sampleList, currentName } = useContext(SampleDataContext)
+
+    const [addMenuOpen, setAddMenuOpen] = useState<boolean>(false)
+    const [ menuAnchor, setMenuAnchor ] = useState<null | HTMLElement>(null);
 
     let currentSample = sampleList.find(i => i.name == currentName)
     if (currentSample == undefined){
         currentSample = {name:currentName as string,
              values:nullSampleValues}
     }
+
+    const toggleAddMenu = (event:null|React.MouseEvent<HTMLButtonElement>=null) => {
+        if (event!=null){
+            setMenuAnchor(event.currentTarget)
+        }
+        else {setMenuAnchor(null)}
+    
+        setAddMenuOpen(!addMenuOpen)
+    } 
 
 
     let initialValues = currentSample.values.filter(i => i.value.val!=null)
@@ -31,12 +44,32 @@ export function EditPropertiesPage () {
 
     return (
         <Stack spacing={2} sx={{m:1,}}>
+
+        <Stack direction="row" sx={{justifyContent:"space-between"}}>
         <Typography align="center">
         <b>Current sample: {currentName}</b>
         </Typography>
+        <Button variant="contained" 
+        onClick={toggleAddMenu}>
         <Stack direction="row" sx={{justifyContent:"center"}}>
             Add Property
             <AddIcon size="small"/>
+        </Stack>
+        </Button>
+        </Stack>
+
+        <Stack>
+        <Menu open={addMenuOpen} onClick={()=>toggleAddMenu()}
+        anchorEl={menuAnchor}>
+        {sampleKeys.map(k => {
+            if (!initialValues.map(i => i.name).includes(k)){
+       
+            return (<MenuItem sx = {{minWidth:"12vw"}}
+            key = {k}>
+                {k}</MenuItem>)
+            }
+        })}
+        </Menu>
         </Stack>
             <Grid container spacing={2}>
                 
