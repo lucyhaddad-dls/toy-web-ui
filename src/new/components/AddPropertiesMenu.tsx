c9cdfb617437064951d03e08720e1c1b1600d7d9
Lucy Haddad <l.haddad@diamond.ac.uk>
2026-09-14 07:20:41 +0000

    fix positioning

--- src/new/components/AddPropertiesMenu.tsx
+++ src/new/components/AddPropertiesMenu.tsx
@@ -7,26 +7,27 @@ import { Button, Menu, MenuItem, Stack } from "@mui/material"
 export function AddPropsMenu(props:{sampleName:string}) {
 
     const [open, setOpen] = useState<boolean>(false)
-    const [position, setPosition] = useState<HTMLElement|null>(null);
     const { sampleList, } = useContext(SampleDataContext)
+    const [top, setTop] = useState<number>(0)
+    const [left, setLeft] = useState<number>(0)
 
     const currentVals = sampleList.find(i => i.name == props.sampleName)?.values.
     find(v => v.name == "formula")
 
     const onMenuClick = (event:React.MouseEvent<HTMLElement>) => {
         setOpen(!open)
-        if (open == false){
-            setPosition(null)
-        }
-        else {setPosition(event.currentTarget)}
-    }
+
+        setTop(event.clientY)
+        setLeft(event.clientX)}
+    
 
     return (
 
         <Stack>
             <Button onClick={onMenuClick}>Add Props.</Button>
-
-            <Menu open={open} onClick={onMenuClick} anchorEl={position}>
+            <Menu open={open} onClick={onMenuClick}
+            anchorReference="anchorPosition"
+            anchorPosition={{ top: top, left: left }}>
             <Stack>
                 <MenuItem>
                 Add Properties 
